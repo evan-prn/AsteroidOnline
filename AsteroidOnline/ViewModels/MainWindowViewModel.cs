8e2fb9b77e718ddfe94861fd7b09b99a1649781e
PERNOT <pernot.evan03@gmail.com>
2026-04-03 08:41:15 +0200

    Initial Commit

---
+++ AsteroidOnline/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AsteroidOnline.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ AsteroidOnline/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AsteroidOnline.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
