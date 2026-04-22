5fd1d14a42ae34a30f37a8965de614a0f966cf52
PERNOT <pernot.evan03@gmail.com>
2026-04-22 14:10:54 +0200

    refactoring: remove unused code and update dependencies

--- src/AsteroidOnline.Client/obj/Debug/net10.0/AsteroidOnline.Client.AssemblyInfo.cs
+++ src/AsteroidOnline.Client/obj/Debug/net10.0/AsteroidOnline.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AsteroidOnline.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad4e8039496af901454793bd3f1456204db6e2e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ae485726aa08a03c8a21612adfc7530d1b95dc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AsteroidOnline.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AsteroidOnline.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
